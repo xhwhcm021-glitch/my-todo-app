db3a13e815cf368da70b4ad1398c10d0c89536df
xhwhcm021-glitch <xhwhcm021@gmail.com>
2026-09-19 18:13:50 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
